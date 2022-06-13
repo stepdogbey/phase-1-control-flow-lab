6c942e4924298558a2818f55820511c3c1374922
stephen <stevedogbey@gmail.com>
2022-06-13 16:12:31 +0000

    lab completed

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let feet = someValue;
+  if(feet <= 400){
+    return "This one is on me!";
+  }
+  else if(feet < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else
+    return 'No can do.';
 }
+scuberGreetingForFeet(100);
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(capCity){
+  return capCity === 'NYC'? "Ok, sounds good." : "No go."
+  // Write your code here
 }
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
+
+
+function switchOnCharmFromTip(appValue){
+  // Write your code here!
+  switch (appValue) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+      break;
+  }
+}
+switchOnCharmFromTip('generous');
+//switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip('thanks for everything');
